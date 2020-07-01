29a8fa1895c73c6e8c243ba0ab3d3676acf50196
Obsecurus <30487781+Obsecurus@users.noreply.github.com>
2020-07-01 10:42:18 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='uhp',
+    version='1.1.1',
+    description='Universal Honeypot.',
+    url="https://github.com/MattCarothers/uhp",
+    author="Matt Carothers",
+    author_email="test@example.com",
+    maintainer = "Matt Carothers",
+    maintainer_email = "test@example.comm",
+    license="BSD",
+    packages=find_packages(),
+    install_requires=['authbind'],
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Unlicensed',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ],
+    package_data={
+        'uhp': [],
+    },
+    entry_points={
+        'console_scripts': [
+            'uhp = uhp:main'
+        ]
+    },
+    keywords=['uhp', 'honeypot', 'universal']
+)
